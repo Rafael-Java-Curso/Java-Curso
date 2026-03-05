dc56b6b70ee00dbae14e63f19d531298644a5df5
Rafael <rafaelgithub25@gmail.com>
2026-03-04 21:27:58 -0500

    case 3

--- modulo-01/class-02/Bucles.java
+++ modulo-01/class-02/Bucles.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Bucles {
     public static void main (String[] args){
         int selOpcion;
+        final float PI = 3.1416f;
+
         Scanner r = new Scanner(System.in);
         do{
             System.out.println("Selecciona una opcion : ");
@@ -17,11 +19,26 @@ public class Bucles {
             selOpcion = r.nextInt();
             switch (selOpcion) {
                 case 1:
-                    System.out.printf("""
+                    System.out.printf(
+                        """
                             Selecciona la figura
                             1. Circulo
                             2. Rectangulo
-                            """);
+                        """);
+                        int opcionUno = r.nextInt();
+                        if(opcionUno == 1){
+                            System.out.println("Ingrese el radio del circulo: ");
+                            float radioCirculo = r.nextFloat();
+                            float areaCirculo = (float) PI * radioCirculo * radioCirculo;
+                            System.out.printf("Area del Circulo = %.3f %n", areaCirculo);
+                        }else{
+                            System.out.println("Ingrese la base del rectangulo : ");
+                            float base = r.nextFloat();
+                            System.out.println("Ingrese la altura del rectangulo");
+                            float altura = r.nextFloat();
+                            float areaRectangulo = base * altura;
+                            System.out.printf("El area del rectangulo es de : %.3f %n", areaRectangulo);
+                        }
                     break;
 
                 case 2:
@@ -29,6 +46,23 @@ public class Bucles {
                     break;
                 
                 case 3:
+                    System.out.println("Digite un numero : ");
+                    int selectNumber = r.nextInt();
+                    boolean ok = true;
+                    if(selectNumber == 1){
+                        ok = false;
+                    }
+                    for(int i = 2; i * i <= selectNumber; i++){
+                        if(selectNumber % i == 0){
+                            ok = false;
+                            break;
+                        }
+                    }
+                    if(ok){
+                        System.out.printf("el numero %d es primo", selectNumber);
+                    }else{
+                        System.out.printf("El numero %d no es primo", selectNumber);
+                    }
                     
                     break;
                 
